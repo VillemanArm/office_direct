95eff47e313b1400ae181b79b732e401237555b3
Belaz4x4 <belaz4x4@yandex.ru>
2023-02-11 17:05:04 +0300

    [upd] add select styles

---
+++ src/js/script.js
@@ -0,0 +1,3 @@
+const selectHeader = document.querySelector(".select__header");
+const selectTitle = document.querySelector(".select__title");
+const selectItem = document.querySelectorAll(".select__item");
