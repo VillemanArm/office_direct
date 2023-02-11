6db791e30e825f7857c02d5481da2c04e845b7e0
Belaz4x4 <belaz4x4@yandex.ru>
2023-02-11 19:19:46 +0300

    [upd] add js to select

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,26 @@
 const selectHeader = document.querySelector(".select__header");
+const selectBody = document.querySelector(".select__body");
 const selectTitle = document.querySelector(".select__title");
 const selectItem = document.querySelectorAll(".select__item");
+const selectImg = document.querySelector(".select__chevron img");
+
+selectHeader.addEventListener("click", () => {
+  if (selectBody.classList.contains("select__body_active")) {
+    selectBody.classList.remove("select__body_active");
+    selectImg.style.transform = "rotate(0deg)";
+    selectBody.style.maxHeight = null;
+  } else {
+    selectBody.classList.add("select__body_active");
+    selectImg.style.transform = "rotate(180deg)";
+    selectBody.style.maxHeight = selectBody.scrollHeight + "px";
+  }
+});
+
+selectItem.forEach((item, i) => {
+  item.addEventListener("click", () => {
+    selectTitle.textContent = item.textContent;
+    selectBody.classList.remove("select__body_active");
+    selectImg.style.transform = "rotate(0deg)";
+    selectBody.style.maxHeight = null;
+  });
+});
