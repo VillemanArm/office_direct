6023dcd5683b85d92dced708b6d9878774e2f83c
Belaz4x4 <belaz4x4@yandex.ru>
2023-02-14 13:22:16 +0300

    add map with javascriptAPI

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+// select
 const selectHeader = document.querySelector(".select__header");
 const selectBody = document.querySelector(".select__body");
 const selectTitle = document.querySelector(".select__title");
@@ -24,3 +25,45 @@ selectItem.forEach((item, i) => {
     selectBody.style.maxHeight = null;
   });
 });
+
+// map
+let center = [55.609685315592216, 37.49508010575369];
+
+function init() {
+  let map = new ymaps.Map("map-element", {
+    center: center,
+    zoom: 15,
+  });
+
+  map.controls.remove("geolocationControl"); // удаляем геолокацию
+  map.controls.remove("searchControl"); // удаляем поиск
+  map.controls.remove("trafficControl"); // удаляем контроль трафика
+  map.controls.remove("typeSelector"); // удаляем тип
+  map.controls.remove("fullscreenControl"); // удаляем кнопку перехода в полноэкранный режим
+  map.controls.remove("zoomControl"); // удаляем контрол зуммирования
+  map.controls.remove("rulerControl"); // удаляем контрол правил
+  map.behaviors.disable(["scrollZoom"]); // отключаем скролл карты
+
+  let placemark = new ymaps.Placemark(
+    center,
+    {
+      balloonContent: `
+        <div class="balloon" style="width: 150px; height: 100px;">
+          <b>ОфисДирект</b><br>
+          Режим работы:<br>
+          ежедневно с 10 до 19 часов
+        </div>
+`,
+    },
+    {
+      iconLayout: "default#image",
+      iconImageHref: "../img/map/placemark.svg",
+      iconImageSize: [70, 100],
+      iconImageOffset: [-30, -100],
+    }
+  );
+
+  map.geoObjects.add(placemark);
+}
+
+ymaps.ready(init);
